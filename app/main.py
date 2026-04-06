8057bdd0407cd31b3830c478273265c71140fd45
Kostiantyn <goncharov.kostiantyn@gmail.com>
2026-04-06 10:55:23 +0300

    fixed bag

--- app/main.py
+++ app/main.py
@@ -58,7 +58,7 @@ class DeliveryDrone(FlyingRobot):
                  weight: int,
                  coords: list | None = None,
                  max_load_weight: int = 0,
-                 current_load: Cargo = None) -> None:
+                 current_load: Cargo | None = None) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
         self.current_load = None
